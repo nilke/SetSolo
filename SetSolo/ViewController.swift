e0d9dc3bf0d3e613d80c38824ecd681406ef5eb1
Nils <nils@oves-macbook-pro.local>
2019-02-10 13:49:44 +0100

    Initial Commit

---
+++ SetSolo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetSolo
+//
+//  Created by Nils on 2019-02-10.
+//  Copyright © 2019 Nils. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
